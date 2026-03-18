78b771dea2f56fd76c296f1df1ef368eb16841ea
Hoang Viet <hoangvietlt05@gmail.com>
2026-03-18 09:48:45 +0700

    Viet test

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b,hieu, tong;
+
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &a);
+
+    printf("Nhap so thu hai: ");
+    scanf("%d", &b);
+
+    tong = a + b;
+	hieu=a-b;
+    printf("Tong = %d\n", tong);
+	printf("Hieu= %d\n", hieu);
+    return 0;
+}
